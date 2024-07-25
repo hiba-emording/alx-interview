e5b06191f3e85f8f94d227c345307f4eda5e87f5
hiba-emording <hibaalaaeldeinmohamed@gmail.com>
2024-07-25 18:28:58 +0300

    pep 8 fix

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -10,7 +10,8 @@ def pascal_triangle(rows):
         rows (int): The number of rows of Pascal's Triangle to generate.
 
     Returns:
-        list of list of int: A list of lists where each inner list represents a row in Pascal's Triangle.
+        list of list of int: A list of lists where each inner list
+        represents a row in Pascal's Triangle.
         If the input is not a positive integer, an empty list is returned.
     '''
 
@@ -24,7 +25,10 @@ def pascal_triangle(rows):
             if col_index == 0 or col_index == row_index:
                 current_row.append(1)
             else:
-                current_row.append(triangle[row_index - 1][col_index - 1] + triangle[row_index - 1][col_index])
+                current_row.append(
+                    triangle[row_index - 1][col_index - 1] + 
+                    triangle[row_index - 1][col_index]
+                )
         triangle.append(current_row)
     
     return triangle
